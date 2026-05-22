fbff96c6918f70a04cb39b6f830aa4485c84145c
kamalvs <61105693+kamalvs@users.noreply.github.com>
2026-05-22 11:30:33 +0530

    Remove forcePathStyle option from S3 client

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -3,7 +3,6 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const s3Client = new S3Client({
   region: process.env.S3_REGION,
-  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY,
     secretAccessKey: process.env.S3_SECRET_KEY,
