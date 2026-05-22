a19956c9377c434476e07fe71731def4c626cf95
kamalvs <61105693+kamalvs@users.noreply.github.com>
2026-05-22 11:49:12 +0530

    Refactor S3 client configuration and upload URL function

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -2,21 +2,24 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const s3Client = new S3Client({
-  region: process.env.S3_REGION,
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_KEY,
-  },
+region: process.env.S3_REGION,
+credentials: {
+accessKeyId: process.env.S3_ACCESS_KEY,
+secretAccessKey: process.env.S3_SECRET_KEY,
+},
+requestChecksumCalculation: "WHEN_REQUIRED",
 });
 
 const getUploadUrl = async (fileName, fileType) => {
-  const command = new PutObjectCommand({
-    Bucket:process.env.S3_BUCKET,
-    Key: fileName,
-    ContentType: fileType,
-  });
+const command = new PutObjectCommand({
+Bucket: process.env.S3_BUCKET,
+Key: fileName,
+ContentType: fileType,
+});
 
-  return await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+return await getSignedUrl(s3Client, command, {
+expiresIn: 3600,
+});
 };
 
 module.exports = { getUploadUrl };
